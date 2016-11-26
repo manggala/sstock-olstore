5ad207f356725fbd9dacfc36378f9f6099d38784
manggala <ghozie.manggala12@mhs.if.its.ac.id>
2016-11-26 15:56:06 +0700

    Route Fixing

--- core/app/Http/routes.php
+++ core/app/Http/routes.php
@@ -1,11 +1,24 @@
 <?php
-Route::resource('/coupon', 'CouponController');
 Route::get('/', function(){
 	return redirect ('/product');
 });
-
+Route::resource('/coupon', 'CouponController');
+Route::get('/coupon/addCoupon/{code}', 'CouponController@addCoupon');
+Route::get('/coupon/deleteCoupon', 'CouponController@deleteCoupon');
 Route::resource('/product', 'ProductController');
 Route::post('/product/addToOrder', 'ProductController@addToOrder');
 
 Route::resource('/bank', 'BankController');
+Route::resource('/order', 'OrderController');
+Route::resource('/validation', 'OrderValidationController');
+Route::get('/validation/validate/{id}/{status}', 'OrderValidationController@validate');
+
+Route::resource('/payment', 'PaymentController');
+Route::post('/payment/checkout', 'PaymentController@checkout');
+Route::post('/payment/confirmationRequest', 'PaymentController@confirmationRequest');
+
+Route::resource('/proof', 'ProofController');
+Route::resource('/user', 'UserController');
+
+
 ?>
